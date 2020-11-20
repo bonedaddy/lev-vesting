34f6c646fe44b4f90a62b45261be5238f65d6c61
bonedaddy <boned4ddy@protonmail.com>
2020-11-19 22:20:31 -0800

    contracts: cleanup vesting code

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -8,7 +8,7 @@ contract Vesting {
 
     uint256 public startTime;
     uint256 public endTime;
-    uint256 public currentRelease;
+    uint256 public currentCycle;
     uint256 public releaseAmount;
     address public receiver;
 
@@ -22,47 +22,81 @@ contract Vesting {
 
     mapping (uint256 => Release) public releases;
 
+    event TokensReleased();
+
     constructor(address _levTokenAddress, address _dateTimeContract) {
         levI = ERC20Interface(_levTokenAddress);
         dateI = DateTimeInterface(_dateTimeContract);
     }
 
-    function prepareVest(uint256 _amountToVest, address _receiver) public {
+    /**
+      * @notice prepares the contract for vesting, depositing tokens and 
+      * @notice marking the address that will be allowed to receive vested funds
+      * @param _amountToVest is the amount of tokens to be vested
+      * @param _receiver is the address that will be allowed to receive the withdrawn funds
+    */
+    function prepare(uint256 _amountToVest, address _receiver) public {
         require(levI.transferFrom(msg.sender, address(this), _amountToVest));
-        startTime = dateI._now();
-        endTime = dateI.addMonths(startTime, 12);
-        releaseAmount = _amountToVest / 12;
-        receiver = _receiver;
-        currentRelease = 1;
-        for (uint i = 1; i <= 12; i++) {
-            releases[i] = Release({
-                timestamp: dateI.addMonths(startTime, i),
-                released: 0
-            });
+        // the current time when vesting starts
+        uint256 _startTime = dateI._now();
+        // the time when vesting ends, and the final token release is allowed
+        uint256 _endTime = dateI.addMonths(_startTime, 12);
+        // set the last token release
+        releases[12].timestamp = _endTime;
+        // now set the other 11 token release times
+        for (uint i = 1; i <= 11; i++) {
+            releases[i].timestamp = dateI.addMonths(_startTime, i);
         }
+        // each month release 1/12 of _amountToVest
+        releaseAmount = _amountToVest / 12;
+        // copy memory variables to storage
+        startTime = _startTime;
+        endTime = _endTime;
+        receiver = _receiver;
+        // set current release cycle
+        currentCycle = 1;
     }
 
+    /**
+        * @notice release funds for the current vesting cycle
+    */
     function release() public {
+        // make sure prepare function has been called and successfully executed
         require(isPrepared() == true);
+        // ensure the current cycle hasn't been released
+        require(releases[currentCycle].released == 0, "already released");
+        // mark current cycle as released
+        releases[currentCycle].released = 1;
+        // get current timestamp
         uint256 timestamp = dateI._now();
-        require(releases[currentRelease].released == 0, "already released");
-        releases[currentRelease].released = 1;
-        require(timestamp >= releases[currentRelease].timestamp, "release timestamp not yet passed");
+        // ensure the current timestamp (date) is on or after the release date
+        require(timestamp >= releases[currentCycle].timestamp, "release timestamp not yet passed");
+        // transfer tokens to designated receiver wallet
         require(levI.transfer(receiver, releaseAmount));
-        currentRelease += 1;
+        // move onto the next cycle
+        currentCycle += 1;
+        // release the tokens
+        emit TokensReleased();
+        
     }
 
-    function isReleased(uint256 _index) public view returns (bool) {
+    /**
+      * @notice returns whether or not the given cycle has released the tokens
+    */
+    function isReleased(uint256 _cycle) public view returns (bool) {
         bool released = false;
-        if (releases[_index].released == 1) {
+        if (releases[_cycle].released == 1) {
             released = true;
         }
         return released;
     }
 
+    /**
+      * @notice returns whether or note the vesting contract has been prepared
+    */
     function isPrepared() public view returns (bool) {
         bool prepared = false;
-        if (receiver != address(0) && releaseAmount > 0 && currentRelease > 0) {
+        if (receiver != address(0) && releaseAmount > 0 && currentCycle > 0) {
             prepared = true;
         }
         return prepared;
